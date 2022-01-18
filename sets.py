ea4f750ad157813c59eea04b606cecf9827296f6
20wh1a1235Ashwitha <96814261+20wh1a1235Ashwitha@users.noreply.github.com>
2022-01-18 10:25:57 +0530

    sets.py
    This code displays the result of respective set operations on the screen.

---
+++ sets.py
@@ -0,0 +1,17 @@
+A = {1, 3, 6, 9, 5, 7}
+B = {0, 2, 5, 4, 6, 8, 9}
+
+print("Union of sets is:")
+print(A | B)
+
+print("Intersection of sets is:")
+print(A & B)
+
+print("Difference of sets is:")
+print(A - B)
+
+print("Symmetric difference of sets is:")
+print(A ^ B)
+        
+
+
